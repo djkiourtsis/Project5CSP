07cfa088b7b6a063c04841ba5394ad6e6e385c6f
Li Li <lli2@wpi.edu>
2016-10-02 05:07:19 -0400

    Reads in the text file input and puts both variables and contraints into an array.

--- src/CSPSolver.java
+++ src/CSPSolver.java
@@ -1,9 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CSPSolver {
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    public static void main(String[] args) throws IOException {
+    	ArrayList<String> inputArr = new ArrayList<String>();
+    	File inputFile = new File(args[0]);
+    	Scanner input = new Scanner(inputFile);
+			while(input.hasNext()){
+				String temp = input.nextLine();
+				if(!temp.startsWith("#")){
+					inputArr.add(temp);
+				}
+			}
+			for(int i = 0; i < inputArr.size(); i++){
+				System.out.println(inputArr.get(i));
+			}
+			input.close();
     }
 
 }
