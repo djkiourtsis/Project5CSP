bdec38caaf300a1ef5d2c61bb2327362133f4152
djkiourtsis <djkiourtsis@wpi.edu>
2016-10-02 17:50:32 -0400

    Update BagSizeConstraint
    Update the BagSizeConstraint class to follow project
    specifications.

--- src/BagSizeConstraint.java
+++ src/BagSizeConstraint.java
@@ -14,7 +14,7 @@ public class BagSizeConstraint extends Constraint {
             for(int j = 0; j < bags.get(i).getItems().size(); j++){
                 bagSize += bags.get(i).getItems().get(j).getWeight();
             }
-            if(bagSize>bags.get(i).getCapacity()){
+            if(bagSize>bags.get(i).getCapacity() || bagSize<Math.floor(bags.get(i).getCapacity()*0.9)){
                 return false;
             }
         }
@@ -23,7 +23,16 @@ public class BagSizeConstraint extends Constraint {
 
     @Override
     public boolean isSatisfiable(ArrayList<Bag> bags) {
-        return isSatisfied(bags);
+        for(int i = 0; i < bags.size(); i++){
+            int bagSize = 0;
+            for(int j = 0; j < bags.get(i).getItems().size(); j++){
+                bagSize += bags.get(i).getItems().get(j).getWeight();
+            }
+            if(bagSize>bags.get(i).getCapacity()){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
